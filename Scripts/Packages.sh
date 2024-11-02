1932412cdaf838ce75bcd75cd9e020d5e91b4c51
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2024-11-02 23:54:55 +0800

    Update Packages.sh
    46~47行

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -43,7 +43,8 @@ UPDATE_PACKAGE "easytier" "lazyoop/networking-artifact" "main" "pkg"
 UPDATE_PACKAGE "vnt" "lazyoop/networking-artifact" "main" "pkg"
 UPDATE_PACKAGE "luci-app-partexp" "sirpdboy/luci-app-partexp" "main"
 UPDATE_PACKAGE "istore" "linkease/istore" "main"
-UPDATE_PACKAGE "luci-app-store" "souwei168/luci-app-store" "main"
+UPDATE_PACKAGE "luci-app-store" "kiddin9/kwrt-packages" "main" "pkg"
+UPDATE_PACKAGE "luci-app-store" "kiddin9/kwrt-packages/tree/main/luci-app-store" "main" "pkg"
 UPDATE_PACKAGE "istore-ui" "linkease/istore-ui" "main"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
