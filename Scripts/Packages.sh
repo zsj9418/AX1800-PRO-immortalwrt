069895822d1c12d64dc4742ecb45186170c4a5e1
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2024-11-07 21:32:58 +0800

    Update Packages.sh
    luci-app-store换源

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -42,7 +42,7 @@ UPDATE_PACKAGE "luci-theme-edge" "ricemices/luci-theme-edge" "master"
 UPDATE_PACKAGE "easytier" "lazyoop/networking-artifact" "main" "pkg"
 UPDATE_PACKAGE "vnt" "lazyoop/networking-artifact" "main" "pkg"
 UPDATE_PACKAGE "luci-app-partexp" "sirpdboy/luci-app-partexp" "main"
-UPDATE_PACKAGE "luci-app-store" "skenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-store" "kiddin9/kwrt-packages" "main" "pkg"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
